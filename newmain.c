9cfed6bc8febf8833bdcb2311f69037ec7cf909e
wifiaircat <chichichi001@knu.ac.kr>
2025-06-21 15:24:46 +0900

    sat

--- newmain.c
+++ newmain.c
@@ -4,16 +4,30 @@
 #include <unistd.h>
 
 int main() {
+    char input[128];
     char command[64];
 
     while (1) {
         printf("> Enter command (mount / wait / umount / exit): ");
-        scanf("%s", command);
+        
+        if (!fgets(input, sizeof(input), stdin)) {
+            printf("[!] Input error.\n");
+            continue;
+        }
+
+        input[strcspn(input, "\n")] = 0;
+        int count = sscanf(input, "%s", command);
+
+        char *space = strchr(input, ' ');
+        if (space && *(space + 1) != '\0') {
+            printf("[!] too many arguments.\n");
+            continue;
+        }
 
         if (strcmp(command, "mount") == 0) {
             system("./mount");
             break;
-        } else if (strcmp(command, "wait") == 0){
+        } else if (strcmp(command, "wait") == 0) {
             system("./wait");
         } else if (strcmp(command, "umount") == 0) {
             system("./umount");
